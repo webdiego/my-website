3a264453404619a644056f910a4c45bf75a38f8f
Diego Massarni <webdiegomassarini@gmail.com>
2024-02-09 09:51:59 +1000

    Add @vercel/speed-insights package and component***
    ***Update layout.tsx to include SpeedInsights component from @vercel/speed-insights/next***
    
    ***Update package.json and yarn.lock with @vercel/speed-insights@^1.0.9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Diego Massarini',
-  description: 'Web | Mobile developer',
+  title: "Diego Massarini",
+  description: "Web | Mobile developer",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
