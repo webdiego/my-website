0cb02a6de182f7a8186181f4191054b630a1eec2
Diego Massarni <webdiegomassarini@gmail.com>
2024-04-12 12:20:43 +1000

    add umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AuroraBackground } from "@/components/AuroraBackground";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -21,6 +22,11 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
+
+        <Script
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id="8612eba9-e3c6-4f3f-9a64-a1128144c3ba"
+        />
       </body>
     </html>
   );
