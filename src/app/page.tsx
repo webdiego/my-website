a340216f07ee1494980eb0b08f991f923ef352b7
Diego Massarni <webdiegomassarini@gmail.com>
2024-07-19 09:39:19 +1000

    update pkgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Description from "@components/Description";
 import Contacts from "@components/Contacts/index";
 import Technologies from "@components/Technologies";
 import Portfolio from "@/components/Portfolio";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Diego Massarini - Frontend Developer",
